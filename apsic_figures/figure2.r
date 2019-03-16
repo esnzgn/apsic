146b17b12c630d5787894c022ca3b2c28a5558ed
Hesam Montazeri <hesam.montazeri@gmail.com>
2019-03-16 18:11:53 +0330

    figure 2

--- apsic_figures/figure2.r
+++ apsic_figures/figure2.r
@@ -1,1 +1,16 @@
-f
+rm(list=ls())
+
+set.seed(10)
+
+prepareData <-function() {
+  data = matrix(runif(n=50*17), 50, 17)
+  
+  rownames(data) =paste0(rep(c("KRAS", "TP53", "MDM2", "MDM4", "GATA3"), 10), 1:15)
+  colnames(data)=paste0("TCGA", 1:17)
+  data
+}
+
+  
+plotHeatmap function <- (data) {
+  
+}
