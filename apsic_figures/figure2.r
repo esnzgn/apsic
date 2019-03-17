2f8679f5ccef6a9886cc0764509203fb9d5aa01d
esnzgn <zangeneh.ehsan@gmail.com>
2019-03-17 15:33:02 +0330

    minor upfate fig 2

--- apsic_figures/figure2.r
+++ apsic_figures/figure2.r
@@ -1,21 +1,32 @@
 rm(list=ls())
+library(pheatmap)
 
 set.seed(10)
 
-prepareData <-function() {
-  data = matrix(runif(n=50*17), 50, 17)
+prepareData <-function(r, c) {
+  data = matrix(runif(n=r*c), r, c)
   
-  rownames(data) =paste0(rep(c("KRAS", "TP53", "MDM2", "MDM4", "GATA3"), 10), 1:15)
-  colnames(data)=paste0("TCGA", 1:17)
+  rownames(data) =paste0(rep(c("KRAS", "TP53", "MDM2", "MDM4", "GATA3"), r/5), 1:c)
+  colnames(data)=paste0("TCGA", 1:c)
   data
 }
 
-  
-plotHeatmap <- function(data) {
-  # heat
-  
-}
 
 
-fakeData = prepareData()
-plotHeatmap(fakeData)
+
+# plotHeatmap <- function(data) {
+#   # heat
+#   
+# }
+
+
+dat = prepareData(20, 20)
+
+# dat = matrix(rnorm(50*17),50,17)
+# heatmap(dat)
+dev.off()
+
+# pheatmap(dat, cluster_rows = FALSE,cluster_cols = FALSE)
+pheatmap(dat, cluster_rows = FALSE,cluster_cols = FALSE,
+        color = colorRampPalette(c("black","firebrick3","white"))(10), 
+        display_numbers = matrix(ifelse(dat > 0.8, "*", ""), nrow(dat)), border_color = FALSE)
