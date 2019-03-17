3b1e87c2ab506c5671d94913aea4e2a3606ace8f
Hesam Montazeri <hesam.montazeri@gmail.com>
2019-03-17 16:19:32 +0330

    minor update

--- apsic_figures/figure3.r
+++ apsic_figures/figure3.r
@@ -16,11 +16,13 @@ plotInChromosomeContext <- function(gene_names, geneAnnot) {
   
   genes = genes[order(genes@seqnames, genes@ranges@start), ]
   
-  u_chros = unique(as.character(genes@seqnames))
-  u_chros = u_chros[order(as.numeric(substring(u_chros, 4)))]
+  char_element = as.character(substring(unique(as.character(genes@seqnames)), 4))
   
-  
-  kp <- plotKaryotype(genome="hg38", plot.type = 2, chromosomes=u_chros)
+  allChromo = c(1:22, "X", "Y")
+  u_chros = allChromo[which(allChromo %in% char_element)]
+
+
+  kp <- plotKaryotype(genome="hg38", plot.type = 2, chromosomes=paste0("chr",u_chros) )
 
   n = length(genes@seqnames)
   
@@ -43,9 +45,56 @@ gene_names = apsic_result$gene
 plotInChromosomeContext(gene_names, geneAnnot)
 
 
-dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-non-genetic-high.csv", stringsAsFactors = FALSE)
-n = nrow(dat)
-apsic_result = dat[which(dat$pvalue_wt < 1/n & dat$tumor_expressed_less < 0.05 ),]
-gene_names = apsic_result$gene
+candidateGenes  <- function(cancer_type ) {
+  
+  # non-genetic high : potential tumor suppressor
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                 "-non-genetic-high.csv"), stringsAsFactors = FALSE)
+  n = nrow(dat)
+  gene_set1 = dat[which(dat$pvalue_wt < 1/n & dat$tumor_expressed_less <0.05), "gene"]
 
-plotInChromosomeContext(gene_names, geneAnnot)
+  # non-genetic low
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                        "-non-genetic-low.csv"), stringsAsFactors = FALSE)
+  gene_set2 = dat[which(dat$pvalue_wt < 1/n & dat$tumor_expressed_more <0.05), "gene"]
+  
+  # missense
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                        "-missense-low.csv"), stringsAsFactors = FALSE)
+  n = sum(dat$freq_mut > 1)
+  gene_set3 = dat[which(dat$pvalue_mut < 1/n & dat$pvalue_wt > 0.05), "gene"]
+
+  # amplification
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                        "-amplification-low.csv"), stringsAsFactors = FALSE)
+  n = sum(dat$freq_mut > 1)
+  gene_set4 = dat[which(dat$pvalue_mut < 1/n & dat$pvalue_wt > 0.05), "gene"]
+  
+  
+  # truncating
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                        "-truncating-high.csv"), stringsAsFactors = FALSE)
+  n = sum(dat$freq_mut > 1)
+  gene_set5 = dat[which(dat$pvalue_mut < 1/n & dat$pvalue_wt > 0.05), "gene"]
+  
+  
+  list(nongen_high=gene_set1, nongen_low=gene_set2, gen_missense=gene_set3, 
+       gen_amplification=gene_set4, gen_truncating=gene_set5)
+}
+
+
+# dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-non-genetic-high.csv", stringsAsFactors = FALSE)
+dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-non-genetic-low.csv", stringsAsFactors = FALSE)
+dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-missense-low.csv", stringsAsFactors = FALSE)
+dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-amplification-low.csv", stringsAsFactors = FALSE)
+dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-truncating-high.csv", stringsAsFactors = FALSE)
+n = nrow(dat)
+# apsic_result = dat[which(dat$tumor_expressed_less < 0.05 ),]
+apsic_result = dat[which(dat$pvalue_wt > 0.05 ),]
+head(apsic_result)
+gene_names = apsic_result$gene[1:40]
+
+cancer_type = "Breast_Carcinoma"
+gene_names = candidateGenes(cancer_type)
+
+plotInChromosomeContext(unlist(gene_names), geneAnnot)
