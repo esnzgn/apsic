0f92b15c13ba719b08b7648c1d67a373cdfdc6f5
Hesam Montazeri <hesam.montazeri@gmail.com>
2019-03-17 16:32:46 +0330

    figure 3

--- apsic_figures/figure3.r
+++ apsic_figures/figure3.r
@@ -5,7 +5,7 @@ library(karyoploteR)
 
 plotInChromosomeContext <- function(gene_names, geneAnnot) {
 
-
+    # compute list of chromosomes which contain gene_names  
   genes_hg38_ = geneAnnot[which(geneAnnot$gene_name %in% gene_names), c("seqnames", "start", "end", "gene_name", "width")]
   genes_hg38 = genes_hg38_[order(genes_hg38_$gene_name, genes_hg38_$width), 1:4]
   
@@ -20,17 +20,18 @@ plotInChromosomeContext <- function(gene_names, geneAnnot) {
   
   allChromo = c(1:22, "X", "Y")
   u_chros = allChromo[which(allChromo %in% char_element)]
-
+  
 
   kp <- plotKaryotype(genome="hg38", plot.type = 2, chromosomes=paste0("chr",u_chros) )
 
   n = length(genes@seqnames)
+
+
+  odds = seq(1, length(genes), by=2)
+  kpPlotMarkers(kp, data=genes[odds, ], labels=genes$gene_name[odds], r1 = 0.6, cex=0.65, text.orientation = "horizontal", data.panel = 1, label.color="#7b3294")
   
-  
-  # ids = setdiff(seq(1, n, 1), cosmic_indexes)
-  # kpPlotMarkers(kp, data=genes[ids, ], labels=genes$hgnc_symbol[ids], r1 = 0.6, cex=0.75, text.orientation = "horizontal", data.panel = 1)
-  
-  kpPlotMarkers(kp, data=genes, labels=genes$gene_name, r1 = 0.6, cex=0.75, text.orientation = "horizontal", data.panel = 1)
+  evens = seq(2, length(genes), by=2)
+  kpPlotMarkers(kp, data=genes[evens, ], labels=genes$gene_name[evens], r1 = 0.6, cex=0.65, text.orientation = "horizontal", data.panel = 2)
 }
 
 
@@ -82,19 +83,10 @@ candidateGenes  <- function(cancer_type ) {
        gen_amplification=gene_set4, gen_truncating=gene_set5)
 }
 
+#############################
 
-# dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-non-genetic-high.csv", stringsAsFactors = FALSE)
-dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-non-genetic-low.csv", stringsAsFactors = FALSE)
-dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-missense-low.csv", stringsAsFactors = FALSE)
-dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-amplification-low.csv", stringsAsFactors = FALSE)
-dat = read.csv("../apsic_shiny/apsic_pvalues/Breast_Carcinoma/Breast_Carcinoma-truncating-high.csv", stringsAsFactors = FALSE)
-n = nrow(dat)
-# apsic_result = dat[which(dat$tumor_expressed_less < 0.05 ),]
-apsic_result = dat[which(dat$pvalue_wt > 0.05 ),]
-head(apsic_result)
-gene_names = apsic_result$gene[1:40]
-
-cancer_type = "Breast_Carcinoma"
+#cancer_type = "Breast_Carcinoma"
+cancer_type = "Liver_HCC"
 gene_names = candidateGenes(cancer_type)
 
 plotInChromosomeContext(unlist(gene_names), geneAnnot)
