c8969ef13175d99d4789b2f9c7b35d94f37f705d
Hesam Montazeri <hesam.montazeri@gmail.com>
2019-04-18 15:59:43 +0430

    add fig2

--- apsic_figures/figure2.1.r
+++ apsic_figures/figure2.1.r
@@ -4,10 +4,7 @@ library(reshape2)
 library(ggplot2)
 library(scales)
 
-
-
 plotHeatmap <- function(dat) {
-  
   dat = dat[rev(rownames(dat)),]
   
   dat.m = melt(dat)
@@ -15,7 +12,7 @@ plotHeatmap <- function(dat) {
   dat.m$pvalue[dat.m$pvalue < 10^(-20)] = 10^(-20)
   handle = ggplot(dat.m, aes(type, gene),font=3) +
     geom_tile(aes(fill = pvalue), color = "white") + 
-    scale_fill_gradientn(values=rescale(c(10^(-20), 10^(-5), 0.001,  1)), colours=c("red3", "red2", "red1",  "white") ) +
+    scale_fill_gradientn(values=rescale(c(10^(-20), 10^(-5), 0.001, 0.01, 0.3,  1)), colours=c("red3", "red2", "red1", "orange", "white",  "white") ) +
     ylab("") +
     xlab("") +
     theme_bw()+
@@ -29,59 +26,115 @@ plotHeatmap <- function(dat) {
 
 }
 
-cancer_type = "Pan_cancer"
-
-dat_missense = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
-                               "-missense-low.csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
-dat_missense <- data.frame(dat_missense)
-# typeof(dat_missense)
-
-dat_missense_sort= dat_missense[order(as.numeric(dat_missense$pvalue_mut)),]
-
-# head(dat_missense_sort)
-# typeof(dat_missense)
-# class(dat_missense)
-
-gene_names = dat_missense_sort[1:30, "gene"]
-# dat = read.csv("../apsic_shiny/apsic_pvalues/Bladder_Carcinoma/Bladder_Carcinoma-missense-low.csv", row.names = 1)
-# # head(dat)
-# 
-# 
-# mis_bladder <- dat[gene_names,"pvalue_wt"]
-
-
-########## gene names & cancer
-
-f_pvalue_vector <- function(gene_names, cancer){
+selectImportantGenesForFeature <- function(feature_type) {
+  cancer_type = "Pan_cancer"
+  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer_type, "/", cancer_type, 
+                                 "-", feature_type, ".csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
+  dat <- data.frame(dat)
   
-  dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer, "/", cancer, 
-                                 "-missense-low.csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
-   dat[gene_names,"pvalue_mut"]
+  dat_sort= dat[order(as.numeric(dat$pvalue_mut)),]
   
+  n = length(which(dat_sort$pvalue_mut <0.05))
+  
+  dat_sort[1:min(50, n) , "gene"]
 }
-# ######
+
+preparePvalueData <- function(feature_type, cancers) {
+  
+  # select genes
+  gene_names = selectImportantGenesForFeature(feature_type)
+  # cancer = "Bladder_Carcinoma"
+  pvalues = matrix(0, length(gene_names), length(cancers))
+  rownames(pvalues) = gene_names
+  colnames(pvalues) = cancers
+  i = 1
+  for(cancer in cancers) {
+    dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer, "/", cancer, 
+                          "-",feature_type, ".csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
+    pvalues[, i] = dat[gene_names,"pvalue_mut"]
+    
+    i = i+1
+  }
+  pvalues = pvalues[order(pvalues[, "Pan_cancer"], decreasing = FALSE), ]
+  # pvalues[is.na(pvalues)] = 1.0  
+  pvalues
+}
+
+selectImportantNongeneticGenes <- function(feature_type, cancers) {
+  gene_names = c()
+  for(cancer in cancers) {
+    dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer, "/", cancer, 
+                          "-",feature_type, ".csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
+    
+    print(cancer)
+    print(head(dat[order(as.numeric(dat$pvalue_wt), decreasing = F), "gene" ], 5) )
+    
+    gene_names = c(gene_names, head(dat[order(as.numeric(dat$pvalue_wt), decreasing = F), "gene" ], 5) )
+  }
+  unique(gene_names)
+  # gene_names
+}
+
+prepareNongeneticPvalueData <- function(feature_type, cancers) {
+  
+  # select genes
+  gene_names = selectImportantNongeneticGenes(feature_type, cancers)
+  pvalues = matrix(0, length(gene_names), length(cancers))
+  rownames(pvalues) = gene_names
+  colnames(pvalues) = cancers
+  i = 1
+  for(cancer in cancers) {
+    dat = read.csv(paste0("../apsic_shiny/apsic_pvalues/", cancer, "/", cancer, 
+                          "-",feature_type, ".csv"), stringsAsFactors = FALSE, row.names = 1,header = TRUE)
+    pvalues[, i] = dat[gene_names,"pvalue_wt"]
+    
+    i = i+1
+  }
+  # pvalues = pvalues[order(pvalues[, "Pan_cancer"], decreasing = FALSE), ]
+  # pvalues[is.na(pvalues)] = 1.0  
+  pvalues
+}
+
 
 cancers = list.files("../apsic_shiny/apsic_pvalues/")
-
-
 cancers = c(cancers[which(cancers == "Pan_cancer")],cancers[-which(cancers == "Pan_cancer")])
 
+feature_type = "missense-low"
 
-# cancer = "Bladder_Carcinoma"
-pvalues = matrix(0, 30, length(cancers))
-rownames(pvalues) = gene_names
-colnames(pvalues) = cancers
-i = 1
-for(cancer in cancers) {
-  pvalues[, i] = f_pvalue_vector(gene_names,cancer)  
-   i = i+1
-}
-pvalue_pan = dat_missense_sort[1:30, "pvalue_mut"]
-# pvalues = cbind(pvalue_pan, pvalue)
-pvalues = pvalues[order(pvalues[, "Pan_cancer"]), decreasing = TRUE),]
+pvalues = preparePvalueData("missense-low", cancers)
+plotHeatmap(pvalues)
+
+pvalues = preparePvalueData("amplification-low", cancers)
+plotHeatmap(pvalues)
+
+pvalues = preparePvalueData("truncating-high", cancers)
 plotHeatmap(pvalues)
 
 
-# dev.off()
+
+library(metap)
+pvalues = prepareNongeneticPvalueData("non-genetic-high", cancers[-which(cancers == "Pan_cancer")])
+combined_pvalues = apply(pvalues, 1, function(x) {
+  res = sumlog(x[is.na(x) == FALSE])
+  res$p
+})
+
+pvalues = pvalues[order(combined_pvalues), ]
+
+pdf("non-genetic-high.pdf", 7, 15)
+plotHeatmap(pvalues)
+dev.off()
 
 
+
+pvalues = prepareNongeneticPvalueData("non-genetic-low", cancers[-which(cancers == "Pan_cancer")])
+combined_pvalues = apply(pvalues, 1, function(x) {
+  res = sumlog(x[is.na(x) == FALSE])
+  res$p
+})
+
+pvalues = pvalues[order(combined_pvalues), ]
+
+pdf("non-genetic-low.pdf", 7, 17)
+plotHeatmap(pvalues)
+dev.off()
